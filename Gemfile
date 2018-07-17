e0e81b6cca49fe42e7e39ebba7aaae70b11c32d1
Peter Jang <peter.x.jang@gmail.com>
2018-07-16 19:58:29 -0500

    Store api key using figaro gem

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ gem 'jbuilder', '~> 2.5'
 # gem 'capistrano-rails', group: :development
 gem 'unirest'
 
+gem 'figaro'
+
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,6 +1,6 @@
 class Api::ArticlesController < ApplicationController
   def index
-    response = Unirest.get("https://newsapi.org/v2/top-headlines?country=ae&apiKey=___")
+    response = Unirest.get("https://newsapi.org/v2/top-headlines?country=ae&apiKey=#{ENV["NEWS_API_KEY"]}")
     render json: response.body["articles"]
 
     # @articles = Article.all
